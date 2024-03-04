ef424bf50b564509aa8a8ff145ba98a69a9dea12
Stefano Iannello <62475974+thetimeste@users.noreply.github.com>
2024-03-04 20:50:14 +0100

    Update AI_generated_game.py

--- AI_generated_game.py
+++ AI_generated_game.py
@@ -6,7 +6,7 @@ import time
 
 #Challenge: ChatGPT Generated Videogame
 #Objective: learn how to interact with ChatGPT to make the best out of generative AI in code development.
-#This simple videogame has been generated almost completely by ChatGPT3.5 with little human fixes when stuck in a error loop.
+#This simple videogame has been generated almost completely by ChatGPT3.5 with little fixes when stuck in an error loop.
 
 
 
